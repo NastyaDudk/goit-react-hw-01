ea781f5eea40dded50ed7fbd4ad31646e14876d8
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2024-03-12 13:52:58 +0400

    ok

--- src/js/pixabay-api.js
+++
@@ -1,1 +0,0 @@
-export const apiKey = '42175181-9f2e4ea0c75ffabf50c3ef9f9';
--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,6 @@
 import { defineConfig } from "vite";
-import glob from "glob";
-import injectHTML from "vite-plugin-html-inject";
-import FullReload from "vite-plugin-full-reload";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === "serve" ? "global" : "_global"]: {},
-    },
-    root: "src",
-    build: {
-      sourcemap: true,
-
-      rollupOptions: {
-        input: glob.sync("./src/*.html"),
-        output: {
-          manualChunks(id) {
-            if (id.includes("node_modules")) {
-              return "vendor";
-            }
-          },
-          entryFileNames: "commonHelpers.js",
-        },
-        external: ["axios"],
-      },
-      outDir: "../dist",
-    },
-    plugins: [injectHTML(), FullReload(["./src/**/**.html"])],
-  };
+export default defineConfig({
+  plugins: [react()],
 });
